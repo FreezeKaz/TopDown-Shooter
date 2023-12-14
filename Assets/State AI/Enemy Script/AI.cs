b74e9c6e2fa0e2af3463824682bdaaac3299b89e
Francesca Attindehou <fifame.francescaa@gmail.com>
2023-12-14 13:51:20 +0100

    :wrench: remove agent

--- Assets/State AI/Enemy Script/AI.cs
+++ Assets/State AI/Enemy Script/AI.cs
@@ -1,20 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
     public Transform player;
-    NavMeshAgent agent;
     Animator anim;
     State currentState;
     // Start is called before the first frame update
     void Start()
     {
-        agent = new NavMeshAgent();
         anim = this.GetComponent<Animator>();
-        currentState = new IdleState(this.gameObject, agent, anim, player);
+        currentState = new IdleState(this.gameObject, anim, player);
     }
 
     // Update is called once per frame
